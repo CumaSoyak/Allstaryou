389e259c37d61af66ed91f5eb552ef41f2326ddc
CumaSoyak <cumasoyak51@gmail.com>
2018-02-16 19:07:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cuma/allstaryou/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.cuma.allstaryou;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+        private FirebaseAuth mAuth;
+        private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth =FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener!=null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
