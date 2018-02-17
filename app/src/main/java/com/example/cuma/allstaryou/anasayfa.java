a93496627646e138929c2681b8fec56e27356b1f
CumaSoyak <cumasoyak51@gmail.com>
2018-02-18 02:25:01 +0300

    altmenu kullanılabilir hale geldi

--- app/src/main/java/com/example/cuma/allstaryou/anasayfa.java
+++ app/src/main/java/com/example/cuma/allstaryou/anasayfa.java
@@ -30,6 +30,7 @@ public class anasayfa extends AppCompatActivity {
             return false;
         }
     };
+    /*Buralar*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
